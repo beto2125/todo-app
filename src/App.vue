c26701410249fba37e6ed8b188ec9ac4a495857a
beto2125 <betobeto60@gmail.com>
2024-02-08 14:38:11 -0600

    added delete function

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,11 @@ const addTodo = () => {
   //  console.log(todos) 
 
 }
+
+const removeTodo = (todo) =>{
+  todos.value = todos.value.filter(t => t !== todo)
+}
+
 </script>
 
 <template>
@@ -72,7 +77,9 @@ const addTodo = () => {
           <div class="todo-content">
             <input type="text" v-model="todo.content" />
           </div>
-
+          <div class = "actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
         </div>
 
       </div>
